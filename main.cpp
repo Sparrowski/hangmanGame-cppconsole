35cbc3b144c782c0f732d0be30b0a2357c82f7d4
Kamil Wroblewski <k.wroblewski54@gmail.com>
2019-12-17 12:07:59 +0100

    Finished simple game

--- main.cpp
+++ main.cpp
@@ -3,18 +3,12 @@
  * Date: 15.12.2019
  * ************************************************/
 #include <iostream>
-#include "Hangman.h"
-int main(){
-	Hangman h(6,10);
-	h.set_start_board(' ');
-	int i = 0;
-	while(i < 18){
-		std::cout << "Current mistake: " << i << std::endl;
-		std::cout << h;
-		h.update_hangman(17);
-		i++;
-	std::cout << std::endl;
-	}
+#include "Game.h"
+#include <string>
 
+int main(){
+	Game game("words.txt");
+
+	game.Play();
 	return 0;
 }
